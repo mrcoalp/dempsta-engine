d807b32465530a06185459d0987c80d9f0b9770f
Marco Pinto <marco.alp120@gmail.com>
2020-03-08 02:50:51 +0000

    Changed build script

--- build.sh
+++ build.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
+# Default is debug build
+BUILD=Debug
+
+while test $# -gt 0; do
+  case "$1" in
+  -d | --debug)
+    BUILD=Debug
+    ;;
+  -r | --release)
+    BUILD=Release
+    ;;
+  -c | --clean)
+    rm -rf build
+    ;;
+  -*)
+    echo "Bad argument: $1"
+    exit 1
+    ;;
+  *)
+    echo "Unknown argument: $1"
+    exit 1
+    ;;
+  esac
+  shift
+done
+
 # Build project
+echo "Building a ${BUILD} version!"
 mkdir build
-cd build || exit
-cmake ..
-cmake --build . --config Debug --target all -- -j 10
+cd build || exit 1
+cmake .. -DCMAKE_BUILD_TYPE=${BUILD}
+cmake --build . --target all -- -j 10
+
+exit 0
