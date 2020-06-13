763fc1735e15e6b9c1b8171ba6099803c0114c4b
Marco Pinto <marco.alp120@gmail.com>
2020-06-13 22:59:20 +0100

    Changed logger patterns

--- engine/include/Core/log.h
+++ engine/include/Core/log.h
@@ -3,83 +3,95 @@
 #include "spdlog/spdlog.h"
 
 namespace de {
-    class Log {
-    public:
-        /**
-         * @brief Initializes log system.
-         */
-        static void Init();
+class Log {
+public:
+    /**
+     * @brief Initializes log system.
+     */
+    static void Init();
 
-        /**
-         * @brief Getter for the engine logger.
-         * @return A reference to the engine logger
-         */
-        inline static auto& GetEngineLogger() { return engineLogger; }
+    /**
+     * @brief Getter for the engine logger.
+     * @return A reference to the engine logger
+     */
+    inline static auto& GetEngineLogger() { return engineLogger; }
 
-        /**
-         * @brief Getter for the application logger.
-         * @return A reference to the application logger
-         */
-        inline static auto& GetApplicationLogger() { return applicationLogger; }
+    /**
+     * @brief Getter for the application logger.
+     * @return A reference to the application logger
+     */
+    inline static auto& GetApplicationLogger() { return applicationLogger; }
 
-        /**
-         * @brief Getter for file logger.
-         * @return A reference to file logger.
-         */
-        inline static auto& GetFileEngineLogger() { return fileEngineLogger; }
+    /**
+     * @brief Getter for file logger.
+     * @return A reference to file logger.
+     */
+    inline static auto& GetFileEngineLogger() { return fileEngineLogger; }
 
-        /**
-          * @brief Getter for file logger.
-          * @return A reference to file logger.
-          */
-        inline static auto& GetFileApplicationLogger() { return fileApplicationLogger; }
+    /**
+     * @brief Getter for file logger.
+     * @return A reference to file logger.
+     */
+    inline static auto& GetFileApplicationLogger() { return fileApplicationLogger; }
 
-    private:
-        static std::shared_ptr<spdlog::logger> engineLogger;
-        static std::shared_ptr<spdlog::logger> applicationLogger;
-        static std::shared_ptr<spdlog::logger> fileEngineLogger;
-        static std::shared_ptr<spdlog::logger> fileApplicationLogger;
-    };
+private:
+    static std::shared_ptr<spdlog::logger> engineLogger;
+    static std::shared_ptr<spdlog::logger> applicationLogger;
+    static std::shared_ptr<spdlog::logger> fileEngineLogger;
+    static std::shared_ptr<spdlog::logger> fileApplicationLogger;
+};
 }  // namespace de
 
 #ifdef NDEBUG
-#define LOG_ENGINE_TRACE(...)       ::de::Log::GetEngineLogger()->trace(__VA_ARGS__);\
-                                    ::de::Log::GetFileEngineLogger()->trace(__VA_ARGS__)
-#define LOG_ENGINE_DEBUG(...)       ::de::Log::GetEngineLogger()->debug(__VA_ARGS__);\
-                                    ::de::Log::GetFileEngineLogger()->debug(__VA_ARGS__)
-#define LOG_ENGINE_INFO(...)        ::de::Log::GetEngineLogger()->info(__VA_ARGS__);\
-                                    ::de::Log::GetFileEngineLogger()->info(__VA_ARGS__)
-#define LOG_ENGINE_WARN(...)        ::de::Log::GetEngineLogger()->warn(__VA_ARGS__);\
-                                    ::de::Log::GetFileEngineLogger()->warn(__VA_ARGS__)
-#define LOG_ENGINE_ERROR(...)       ::de::Log::GetEngineLogger()->error(__VA_ARGS__);\
-                                    ::de::Log::GetFileEngineLogger()->error(__VA_ARGS__)
-#define LOG_ENGINE_CRITICAL(...)    ::de::Log::GetEngineLogger()->critical(__VA_ARGS__);\
-                                    ::de::Log::GetFileEngineLogger()->critical(__VA_ARGS__)
+#define LOG_ENGINE_TRACE(...)                         \
+    ::de::Log::GetEngineLogger()->trace(__VA_ARGS__); \
+    ::de::Log::GetFileEngineLogger()->trace(__VA_ARGS__)
+#define LOG_ENGINE_DEBUG(...)                         \
+    ::de::Log::GetEngineLogger()->debug(__VA_ARGS__); \
+    ::de::Log::GetFileEngineLogger()->debug(__VA_ARGS__)
+#define LOG_ENGINE_INFO(...)                         \
+    ::de::Log::GetEngineLogger()->info(__VA_ARGS__); \
+    ::de::Log::GetFileEngineLogger()->info(__VA_ARGS__)
+#define LOG_ENGINE_WARN(...)                         \
+    ::de::Log::GetEngineLogger()->warn(__VA_ARGS__); \
+    ::de::Log::GetFileEngineLogger()->warn(__VA_ARGS__)
+#define LOG_ENGINE_ERROR(...)                         \
+    ::de::Log::GetEngineLogger()->error(__VA_ARGS__); \
+    ::de::Log::GetFileEngineLogger()->error(__VA_ARGS__)
+#define LOG_ENGINE_CRITICAL(...)                         \
+    ::de::Log::GetEngineLogger()->critical(__VA_ARGS__); \
+    ::de::Log::GetFileEngineLogger()->critical(__VA_ARGS__)
 
-#define LOG_TRACE(...)              ::de::Log::GetApplicationLogger()->trace(__VA_ARGS__);\
-                                    ::de::Log::GetFileApplicationLogger()->trace(__VA_ARGS__)
-#define LOG_DEBUG(...)              ::de::Log::GetApplicationLogger()->debug(__VA_ARGS__);\
-                                    ::de::Log::GetFileApplicationLogger()->debug(__VA_ARGS__)
-#define LOG_INFO(...)               ::de::Log::GetApplicationLogger()->info(__VA_ARGS__);\
-                                    ::de::Log::GetFileApplicationLogger()->info(__VA_ARGS__)
-#define LOG_WARN(...)               ::de::Log::GetApplicationLogger()->warn(__VA_ARGS__);\
-                                    ::de::Log::GetFileApplicationLogger()->warn(__VA_ARGS__)
-#define LOG_ERROR(...)              ::de::Log::GetApplicationLogger()->error(__VA_ARGS__);\
-                                    ::de::Log::GetFileApplicationLogger()->error(__VA_ARGS__)
-#define LOG_CRITICAL(...)           ::de::Log::GetApplicationLogger()->critical(__VA_ARGS__);\
-                                    ::de::Log::GetFileApplicationLogger()->critical(__VA_ARGS__)
+#define LOG_TRACE(...)                                     \
+    ::de::Log::GetApplicationLogger()->trace(__VA_ARGS__); \
+    ::de::Log::GetFileApplicationLogger()->trace(__VA_ARGS__)
+#define LOG_DEBUG(...)                                     \
+    ::de::Log::GetApplicationLogger()->debug(__VA_ARGS__); \
+    ::de::Log::GetFileApplicationLogger()->debug(__VA_ARGS__)
+#define LOG_INFO(...)                                     \
+    ::de::Log::GetApplicationLogger()->info(__VA_ARGS__); \
+    ::de::Log::GetFileApplicationLogger()->info(__VA_ARGS__)
+#define LOG_WARN(...)                                     \
+    ::de::Log::GetApplicationLogger()->warn(__VA_ARGS__); \
+    ::de::Log::GetFileApplicationLogger()->warn(__VA_ARGS__)
+#define LOG_ERROR(...)                                     \
+    ::de::Log::GetApplicationLogger()->error(__VA_ARGS__); \
+    ::de::Log::GetFileApplicationLogger()->error(__VA_ARGS__)
+#define LOG_CRITICAL(...)                                     \
+    ::de::Log::GetApplicationLogger()->critical(__VA_ARGS__); \
+    ::de::Log::GetFileApplicationLogger()->critical(__VA_ARGS__)
 #else
 #define LOG_ENGINE_TRACE(...)
 #define LOG_ENGINE_DEBUG(...)
 #define LOG_ENGINE_INFO(...)
-#define LOG_ENGINE_WARN(...)        ::de::Log::GetFileEngineLogger()->warn(__VA_ARGS__)
-#define LOG_ENGINE_ERROR(...)       ::de::Log::GetFileEngineLogger()->error(__VA_ARGS__)
-#define LOG_ENGINE_CRITICAL(...)    ::de::Log::GetFileEngineLogger()->critical(__VA_ARGS__)
+#define LOG_ENGINE_WARN(...) ::de::Log::GetFileEngineLogger()->warn(__VA_ARGS__)
+#define LOG_ENGINE_ERROR(...) ::de::Log::GetFileEngineLogger()->error(__VA_ARGS__)
+#define LOG_ENGINE_CRITICAL(...) ::de::Log::GetFileEngineLogger()->critical(__VA_ARGS__)
 
 #define LOG_TRACE(...)
 #define LOG_DEBUG(...)
 #define LOG_INFO(...)
-#define LOG_WARN(...)               ::de::Log::GetFileApplicationLogger()->warn(__VA_ARGS__)
-#define LOG_ERROR(...)              ::de::Log::GetFileApplicationLogger()->error(__VA_ARGS__)
-#define LOG_CRITICAL(...)           ::de::Log::GetFileApplicationLogger()->critical(__VA_ARGS__)
+#define LOG_WARN(...) ::de::Log::GetFileApplicationLogger()->warn(__VA_ARGS__)
+#define LOG_ERROR(...) ::de::Log::GetFileApplicationLogger()->error(__VA_ARGS__)
+#define LOG_CRITICAL(...) ::de::Log::GetFileApplicationLogger()->critical(__VA_ARGS__)
 #endif
--- engine/src/Core/log.cpp
+++ engine/src/Core/log.cpp
@@ -1,36 +1,33 @@
 #include "Core/log.h"
 
-#include "spdlog/sinks/daily_file_sink.h"
-#include "spdlog/sinks/basic_file_sink.h"
-#include "spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/async.h"
+#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/daily_file_sink.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
 
 namespace de {
-    std::shared_ptr<spdlog::logger> Log::engineLogger;
-    std::shared_ptr<spdlog::logger> Log::applicationLogger;
-    std::shared_ptr<spdlog::logger> Log::fileEngineLogger;
-    std::shared_ptr<spdlog::logger> Log::fileApplicationLogger;
+std::shared_ptr<spdlog::logger> Log::engineLogger;
+std::shared_ptr<spdlog::logger> Log::applicationLogger;
+std::shared_ptr<spdlog::logger> Log::fileEngineLogger;
+std::shared_ptr<spdlog::logger> Log::fileApplicationLogger;
 
-    void Log::Init() {
-        spdlog::set_pattern("%^[%t][%H:%M:%S.%e][%n]: %v%$");
-        engineLogger = spdlog::stdout_color_mt("DEMPSTA");
-        engineLogger->set_level(spdlog::level::trace);
+void Log::Init() {
+    engineLogger = spdlog::stdout_color_mt("DEMPSTA");
+    engineLogger->set_level(spdlog::level::trace);
+    engineLogger->set_pattern("%^[%t][%H:%M:%S.%e][ENGINE]: %v%$");
 
-        applicationLogger = spdlog::stdout_color_mt("APP");
-        applicationLogger->set_level(spdlog::level::trace);
+    applicationLogger = spdlog::stdout_color_mt("APP");
+    applicationLogger->set_level(spdlog::level::trace);
+    applicationLogger->set_pattern("%^[%t][%H:%M:%S.%e]: %v%$");
 
-        fileEngineLogger = spdlog::daily_logger_mt<spdlog::async_factory>(
-                "_DEMPSTA_",
-                "logs/dempsta.log"
-        );
-        fileEngineLogger->set_level(spdlog::level::trace);
+    fileEngineLogger = spdlog::daily_logger_mt<spdlog::async_factory>("DEMPSTA_", "logs/dempsta.log");
+    fileEngineLogger->set_level(spdlog::level::trace);
+    fileEngineLogger->set_pattern("%^[%t][%H:%M:%S.%e][ENGINE]: %v%$");
 
-        fileApplicationLogger = spdlog::daily_logger_mt<spdlog::async_factory>(
-                "_APP_",
-                "logs/dempsta.log"
-        );
-        fileApplicationLogger->set_level(spdlog::level::trace);
+    fileApplicationLogger = spdlog::daily_logger_mt<spdlog::async_factory>("APP_", "logs/dempsta.log");
+    fileApplicationLogger->set_level(spdlog::level::trace);
+    fileApplicationLogger->set_pattern("%^[%t][%H:%M:%S.%e]: %v%$");
 
-        spdlog::flush_every(std::chrono::seconds(1));
-    }
+    spdlog::flush_every(std::chrono::seconds(1));
+}
 }  // namespace de
